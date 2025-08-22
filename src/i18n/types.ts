cd2bab2e27814af22e5dd100e168d76ada216561
OmarLawaty <omarlawatey@gmail.com>
2025-08-22 19:37:09 +0300

    feat: Implement react components replacing & options type safety

--- src/i18n/types.ts
+++ src/i18n/types.ts
@@ -9,4 +9,31 @@ export type I18nKey = keyof EN & keyof AR;
 export type LanguageTranslations = EN | AR;
 export type Translation = Record<string, string>;
 
-export type TranslateOptions = Record<string, string | number | boolean | undefined>;
+type Trim<S extends string> = S extends ` ${infer R}` ? Trim<R> : S extends `${infer R} ` ? Trim<R> : S;
+
+type StripTrailingSlash<S extends string> = S extends `${infer R}/` ? StripTrailingSlash<R> : S;
+
+type CleanTag<S extends string> = Trim<StripTrailingSlash<Trim<S>>>;
+
+export type ExtractPlaceholders<S extends string> = S extends `${string}{${infer K}}${infer Rest}`
+  ? K | ExtractPlaceholders<Rest>
+  : never;
+
+export type ExtractTags<S extends string> =
+  S extends `${string}<${infer Open}>${infer Mid}</${infer Close}>${infer Rest}`
+    ? CleanTag<Open> | CleanTag<Close> | ExtractTags<Mid> | ExtractTags<Rest>
+    : S extends `${string}<${infer Raw}/>${infer Rest2}`
+      ? CleanTag<Raw> | ExtractTags<Rest2>
+      : S extends `${string}<${infer Raw2} />${infer Rest3}`
+        ? CleanTag<Raw2> | ExtractTags<Rest3>
+        : never;
+
+type Lang = keyof Resources;
+type KeysOf<L extends Lang> = keyof Resources[L] & string;
+type TextOf<L extends Lang, K extends KeysOf<L>> = Resources[L][K] & string;
+
+export type TranslateOptions<L extends Lang, K extends KeysOf<L>> = {
+  [P in ExtractPlaceholders<TextOf<L, K>>]: string | number;
+} & {
+  [T in ExtractTags<TextOf<L, K>>]: (chunks: string) => React.ReactNode;
+};
