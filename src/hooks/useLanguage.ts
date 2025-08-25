7962680b7f6c3ed9194fa99b3032c9ec5a430f69
OmarLawaty <omarlawatey@gmail.com>
2025-08-25 17:53:36 +0300

    feat: Force app restart to change app direction based on the language

--- src/hooks/useLanguage.ts
+++ src/hooks/useLanguage.ts
@@ -1,8 +1,23 @@
+import * as Updates from 'expo-updates'; // only if you use Expo
+import { I18nManager } from 'react-native';
+
 import { useAppStore } from '@/stores';
 
 export const useLanguage = () => {
-  const setLanguage = useAppStore((state) => state.setLanguage);
-  const language = useAppStore((state) => state.language);
+  const changeLanguage = useAppStore((state) => state.setLanguage);
+  const currentLanguage = useAppStore((state) => state.language);
 
-  return { setLanguage, language };
+  const setLanguage = async (language: Language) => {
+    if (currentLanguage === language) return;
+
+    const isAr = language === 'ar';
+    I18nManager.allowRTL(isAr);
+    I18nManager.forceRTL(isAr);
+    I18nManager.swapLeftAndRightInRTL(isAr);
+
+    changeLanguage(language);
+    await Updates.reloadAsync();
+  };
+
+  return { setLanguage, language: currentLanguage, isAr: currentLanguage === 'ar' };
 };
